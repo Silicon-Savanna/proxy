7ba8c43e4c5ba62399b7b9158d2b06dca7f48ad4
Timothy Ngorima <timothytakudzwa@gmail.com>
2023-07-19 01:25:39 +0200

    Add profile and wa_id

--- proxy/views.py
+++ proxy/views.py
@@ -8,11 +8,14 @@ class ProxyView(APIView):
         
         # convert request.data to json
         entry = json.loads(request.body)
-        name = entry['entry'][0]['changes'][0]['value']['contacts'][0]['profile']['name']
-        phone_number = entry['entry'][0]['changes'][0]['value']['contacts'][0]['wa_id']
-        
-        message_body = entry['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
         message_type = entry['entry'][0]['changes'][0]['value']['messages'][0]['type']
+        print(entry)
+        if message_type == 'text':
+            name = entry['entry'][0]['changes'][0]['value']['contacts'][0]['profile']['name']
+            phone_number = entry['entry'][0]['changes'][0]['value']['contacts'][0]['wa_id']
+            
+            message_body = entry['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
+        
 
         print(f'phone_number: {phone_number}')
         print(f'phone_number: {name}')
