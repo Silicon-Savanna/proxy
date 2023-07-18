8e9f265dd5de66cd6fe202c6b6df31485e8dd6f0
Timothy Ngorima <timothytakudzwa@gmail.com>
2023-07-19 00:57:40 +0200

    Fix TypeError: 'module' object is not callable

--- proxy/views.py
+++ proxy/views.py
@@ -4,9 +4,9 @@ import pprint
 
 class ProxyView(APIView):       
     def post(self, request):
-        pprint(request.data)
+        
         # convert request.data to json
         json_data = request.data.get('json')
-        pprint(json_data)
+        pprint.pprint(json_data)
         
         return Response({'hello': 'world'})
