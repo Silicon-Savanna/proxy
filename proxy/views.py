9abab34c43dd6e7600ecf5557bb39325707a42a2
Timothy Ngorima <timothytakudzwa@gmail.com>
2023-07-19 01:03:28 +0200

    Print

--- proxy/views.py
+++ proxy/views.py
@@ -1,12 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import pprint
+import json
 
 class ProxyView(APIView):       
     def post(self, request):
         
         # convert request.data to json
-        json_data = request.data.get('json')
-        pprint.pprint(json_data)
+        entry = json.loads(request.data['entry'][0])
+        phone_number = entry['changes'][0]['value']['metadata']['display_phone_number']
+        message_body = entry['changes'][0]['value']['messages'][0]['text']['body']
+        message_type = entry['changes'][0]['value']['messages'][0]['type']
+        print(f'phone_number: {phone_number}')
+        print(f'message_body: {message_body}')
+        print(f'message_type: {message_type}')
+        print(f'entry: {entry}')
         
         return Response({'hello': 'world'})
