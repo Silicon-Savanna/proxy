05fa2a0642ae5b2d7d6a43446a1f8c6d7e9532cb
Timothy Ngorima <timothytakudzwa@gmail.com>
2023-07-19 00:47:28 +0200

    Add Start app bash file

--- proxy/settings.py
+++ proxy/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+ALLOWED_HOSTS = ['*']
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
