6940fda56da8ac61cf1c831ee884a96bccfdfd5b
Timothy Ngorima <timothytakudzwa@gmail.com>
2023-07-19 00:56:16 +0200

    Add pprint

--- proxy/views.py
+++ proxy/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import pprint
 
 class ProxyView(APIView):       
     def post(self, request):
-        # get payload from request at json
-
-        print(request)
-        print(request.data)
+        pprint(request.data)
+        # convert request.data to json
+        json_data = request.data.get('json')
+        pprint(json_data)
+        
         return Response({'hello': 'world'})
