9e4a7cea2260c021e6e5e2ace9f404c8a7ae53ea
Timothy Ngorima <timothytakudzwa@gmail.com>
2023-07-19 23:54:12 +0200

    Get media Url

--- proxy/views.py
+++ proxy/views.py
@@ -67,7 +67,7 @@ def get_media_url(media_id):
     response = requests.request("GET", url, headers=headers, data=payload)
     # response to json 
     if response.status_code == 200:
-        response = response.json()
+        
         print(response['url'])
     else:
         return None
