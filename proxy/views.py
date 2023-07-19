74897943b7c2b2f6d23f1002960866ffe87bf099
Timothy Ngorima <timothytakudzwa@gmail.com>
2023-07-19 02:02:40 +0200

    Trying to get the actual image

--- proxy/views.py
+++ proxy/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-import pprint
+import requests
 import json
 
 class ProxyView(APIView):       
@@ -21,9 +21,24 @@ class ProxyView(APIView):
             print(f'message_type: {message_type}')
         
         if message_type == 'image':
+            
             image = entry['entry'][0]['changes'][0]['value']['messages'][0]['image']
+            get_media_url(image['id'])
             print(image)
         
         
         
         return Response({'hello': 'world'})
+    
+def get_media_url(media_id):    
+    url = f"https://graph.facebook.com/v17.0/{media_id}"
+
+    payload = {}
+    headers = {
+    'Authorization': 'Bearer EAAIZAebVGy5sBAHqQQDm0dG3a9KQObZA7pfoiN0qVHCqzBkSFjmOt6UMGkvKSQiUBvJ9cpLfK2qp4Prc9YOnl6KRnZBhxiZCZB8wROkdNuHAUxbaPUnyKWud555ia1n09ZB8VYQrE3sXluV3ZCS9yZCtflH1kbBKD7lxKkpwbhSfva4V4utMeP9Q'
+    }
+    
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.data)
+    print(json.loads(response.data))
