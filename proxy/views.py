44f77ddecfb4da9de9138a351f6e16bed2c9864b
Timothy Ngorima <tngorima@cimas.co.zw>
2023-10-04 09:29:35 +0200

    Add Interactive Messages

--- proxy/views.py
+++ proxy/views.py
@@ -44,6 +44,9 @@ class ProxyView(APIView):
             elif message_type == "audio":
                 message_body = entry["entry"][0]["changes"][0]["value"]["messages"][0]["audio"]["id"]
                 media_url = get_media_url(message_body)
+            elif message_type == "interactive":
+                message_body = entry["entry"][0]["changes"][0]["value"]["messages"][0]["interactive"]['button_reply']["id"]
+                media_url = get_media_url(message_body)
             else:
                 return Response({"error": "Invalid message type"}, status=400)
 
