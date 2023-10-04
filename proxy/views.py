f7600417e0db52ffacec9f16f44a9896f5ee4be3
Timothy Ngorima <tngorima@cimas.co.zw>
2023-10-04 09:41:26 +0200

    Payload

--- proxy/views.py
+++ proxy/views.py
@@ -47,7 +47,7 @@ class ProxyView(APIView):
             elif message_type == "interactive":
                 print(entry["entry"][0]["changes"][0]["value"]["messages"][0]["interactive"]["button_reply"])
                 message_body = entry["entry"][0]["changes"][0]["value"]["messages"][0]["interactive"]['button_reply']["id"]
-                # media_url = get_media_url(message_body)
+                media_url = None
             else:
                 return Response({"error": "Invalid message type"}, status=400)
 
